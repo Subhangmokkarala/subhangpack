bc794270b364854b5ffe4ceec2e286c6a4618889
Subhang Mokkarala <90596655+Subhangmokkarala@users.noreply.github.com>
2024-01-19 19:08:09 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Youre too early'
+
+# Setting up
+setup(
+    name="fitzingoutpkg",
+    version=VERSION,
+    author="SubhangMokkarala",
+    author_email="<subhangmokkarala@gmail>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'tensorflow'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: Learning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
